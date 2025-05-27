4bc8c1f69f181a3fa6c9f6c9434798d350c90176
AndrezeraDev <andrezera.dev@gmail.com>
2025-05-26 22:59:54 -0300

    adição da funcionalidade de riscar tarefas, icone

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,22 @@ function App() {
     isCompleted: false
   }])
 
+  function onTaskClick(taskId){
+    const newTask = task.map(task => {
+      if(task.id === taskId){
+        return {...task, isCompleted: !task.isCompleted}
+      }
+      return task;
+    });
+    setTasks(newTask)
+  }
+
   return(
     <div className='w-screen h-screen bg-slate-500 flex justify-center p-6'>
       <div className='w-[500px]'>
         <h1 className='text-3xl text-slate-100 font-bold text-center'>Gerenciador de Tarefas</h1>
         {/* <AddTask /> */}
-        <Tasks tasks={task}/>
+        <Tasks tasks={task} onTaskClick={onTaskClick}/>
       </div>
     </div>
   )
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,12 +1,19 @@
+import { ChevronRightIcon } from "lucide-react";
 
 
 function Tasks(props){
     return(
         <ul className="space-y-4 p-6 rounded-md bg-slate-200 shadow">
             {props.tasks.map((task) => (
-            <li key={task.id} className="flex">
-            <button className="bg-slate-400 text-white p-2 rounded-md w-full">{task.title}</button>
-            <button>Ver detalhes</button>
+            <li key={task.id} className="flex gap-2">
+                <button 
+                onClick={() => props.onTaskClick(task.id)} 
+                className={`bg-slate-400 text-white text-left p-2 rounded-md w-full ${task.isCompleted && 'line-through'}`}>
+                {task.title}
+                </button>
+                <button >
+                    <ChevronRightIcon  className="bg-slate-400 p-2 rounded-md text-white" />
+                </button>
             </li>
             
         ))}</ul>
